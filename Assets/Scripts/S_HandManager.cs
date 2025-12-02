33672d250f463b8c169c7c4709d4e42d0cc98c75
Aurcereal <aidannn@seas.upenn.edu>
2025-12-01 20:22:39 -0500

    Random Objects

--- Assets/Scripts/S_HandManager.cs
+++ Assets/Scripts/S_HandManager.cs
@@ -21,12 +21,11 @@ public class S_HandManager : MonoBehaviour
 
     public void SpawnObject(Vector3 pos, Vector3 dir)
     {
-        var o = Instantiate(SpawnableObjects[currentSpawnable], pos, Quaternion.LookRotation(dir, Vector3.up));
+        var o = Instantiate(SpawnableObjects[UnityEngine.Random.Range(0, SpawnableObjects.Length)], pos, Quaternion.LookRotation(dir, Vector3.up));
         foreach(var rb in o.GetComponentsInChildren<Rigidbody>())
         {
             rb.AddForce(dir*10.0f, ForceMode.Impulse);
         }
-        currentSpawnable = (currentSpawnable+1) % SpawnableObjects.Length;
 
         Material mat = ObjectMaterials[UnityEngine.Random.Range(0, ObjectMaterials.Length)];
         foreach(var mr in o.GetComponentsInChildren<MeshRenderer>())
@@ -34,5 +33,4 @@ public class S_HandManager : MonoBehaviour
             mr.material = mat;
         }
     }
-    int currentSpawnable = 0;
 }
